82475cc1cb26e47735628046dbaf619a96e02066
Jeremy Lach <lach.jeremy@gmail.com>
2023-03-04 18:26:18 -0700

    fdsfs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,33 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/main.module.scss";
 
 // import './App.css';
 
 function App() {
+
+  const [code, setCode] = useState<string>('');
+
   return (
     <div>
-      <div className="container flex flex-col justify-center min-h-screen mx-auto">
+      <div className="flex flex-col justify-center min-h-screen mx-auto max-w-[50vw]">
         <div className="flex flex-col justify-center gap-y-5">
           <div className="flex flex-row space-x-4">
             <span className="text-2xl">👛</span>
             <select className="flex-grow rounded-md bg-blue-200 text-slate-700 font-mono p-2 font-bold">
-              <option v-for="(a,i) in addresses value='a'">a</option>
+              <option>a</option>
             </select>
           </div>
           <textarea
             id="editor"
             className="bg-slate-700 text-slate-100 rounded-md p-4"
             v-model="code"
+            value={code}
+            onChange={(e) => {
+              setCode(e.target.value);
+            }}
           ></textarea>
           <div className="flex justify-between">
             <div className="no-outline flex space-x-2 focus:outline-none bg-green-200 rounded-md py-2 px-5 font-bold text-slate-700">
@@ -32,7 +39,7 @@ function App() {
               <span>ETH</span>
             </div>
             <button className="bg-yellow-400 rounded-full py-2 px-5 font-bold text-slate-700">
-              Execute
+              Execute!
             </button>
           </div>
         </div>
